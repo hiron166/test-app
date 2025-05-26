c0503f87a795d1fd9f346dd927eba667c401b988
hiromu <mi326hi@gmail.com>
2025-05-27 02:29:12 +0900

    task_1 commit

--- src/App.js
+++ src/App.js
@@ -4,24 +4,44 @@ import { posts } from "./data/posts";
 export const App = () => {
   return (
     <>
-      <container>
-        <ul>
-          {posts.map((post) => (
-            <li key={post.id} className="flex flex-col text-[#333]">
-              <div className="mb-8 border p-4 border-gray-300">
-                <div className="flex justify-between">
-                  <div className="text-xs text-[#888]">{new Date(post.createdAt).toLocaleDateString()}</div>
-                  <div className="border rounded text-[#06c] border-[#06c]">
-                    {post.categories}
+      <header className="flex justify-between p-6 bg-[#333] text-[#fff] font-bold">
+        <a href="/">Blog</a>
+        <a href="/">お問い合わせ</a>
+      </header>
+      <div>
+        <div className="my-8 mx-auto max-w-3xl px-4">
+          <ul>
+            {posts.map((post) => (
+              <li key={post.id} className="flex flex-col  text-[#333] ">
+                <a href={`/posts/${post.id}`}>
+                  <div className=" mb-8 border p-4 border-[#ccc]">
+                    <div className="flex justify-between">
+                      <div className="text-xs text-[#888]">
+                        {new Date(post.createdAt).toLocaleDateString()}
+                      </div>
+                      <div className="flex">
+                        {post.categories.map((category) => (
+                          <div
+                            key={category}
+                            className="border rounded text-[#06c] border-[#06c] mr-2 px-1 py-0.5"
+                          >
+                            {category}
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                    <p className="text-2xl mt-2 mb-4">{`APIで取得した${post.title}`}</p>
+                    <div
+                      dangerouslySetInnerHTML={{ __html: post.content }}
+                      className="line-clamp-2"
+                    />
                   </div>
-                </div>
-                <p>{`APIで取得した${post.title}`}</p>
-                <div>{post.content}</div>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </container>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </>
   );
 };
