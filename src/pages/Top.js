b47f2a4727df456e42c40e890dbcda15c2575151
hiromu <mi326hi@gmail.com>
2025-06-08 15:09:36 +0900

    読み込み中表示　実装

--- src/pages/Top.js
+++ src/pages/Top.js
@@ -5,54 +5,70 @@ import { Link } from "react-router-dom";
 
 export const Top = () => {
   const [post, setPost] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetcher = async () => {
-      const res = await fetch(
-        "https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts"
-      );
-      const data = await res.json();
-      setPost(data);
+      try {
+        const res = await fetch(
+          "https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts"
+        );
+        const data = await res.json();
+        setPost(data);
+      } catch (e) {
+        console.log(e);
+      } finally {
+        setIsLoading(false);
+      }
     };
 
     fetcher();
   }, []);
-  return (
-    <>
-      <div>
-        <div className="my-8 mx-auto max-w-3xl px-4">
-          <ul>
-            {posts.map((post) => (
-              <li key={post.id} className="flex flex-col  text-[#333] ">
-                <Link to={`/posts/${post.id}`}>
-                  <div className=" mb-8 border p-4 border-[#ccc]">
-                    <div className="flex justify-between">
-                      <div className="text-xs text-[#888]">
-                        {new Date(post.createdAt).toLocaleDateString()}
+
+  if (isLoading) {
+    return (
+      <>
+        <p className="my-8 mx-auto max-w-3xl px-4">読み込み中...</p>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div>
+          <div className="my-8 mx-auto max-w-3xl px-4">
+            <ul>
+              {posts.map((post) => (
+                <li key={post.id} className="flex flex-col  text-[#333] ">
+                  <Link to={`/posts/${post.id}`}>
+                    <div className=" mb-8 border p-4 border-[#ccc]">
+                      <div className="flex justify-between">
+                        <div className="text-xs text-[#888]">
+                          {new Date(post.createdAt).toLocaleDateString()}
+                        </div>
+                        <div className="flex">
+                          {post.categories.map((category) => (
+                            <div
+                              key={category}
+                              className="border rounded text-[#06c] border-[#06c] mr-2 px-1 py-0.5"
+                            >
+                              {category}
+                            </div>
+                          ))}
+                        </div>
                       </div>
-                      <div className="flex">
-                        {post.categories.map((category) => (
-                          <div
-                            key={category}
-                            className="border rounded text-[#06c] border-[#06c] mr-2 px-1 py-0.5"
-                          >
-                            {category}
-                          </div>
-                        ))}
-                      </div>
+                      <p className="text-2xl mt-2 mb-4">{`APIで取得した${post.title}`}</p>
+                      <div
+                        dangerouslySetInnerHTML={{ __html: post.content }}
+                        className="line-clamp-2"
+                      />
                     </div>
-                    <p className="text-2xl mt-2 mb-4">{`APIで取得した${post.title}`}</p>
-                    <div
-                      dangerouslySetInnerHTML={{ __html: post.content }}
-                      className="line-clamp-2"
-                    />
-                  </div>
-                </Link>
-              </li>
-            ))}
-          </ul>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 };
